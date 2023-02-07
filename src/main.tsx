a41f61dec602ab0a67e09c264a1fe4c545b8fbd8
D3press3d <anthonyrosman20@gmail.com>
2023-02-07 12:56:53 -0400

    chore: provide the redux store

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './store/store'
 import MainBlog from './MainBlog'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <MainBlog />
+    <Provider store={store}>
+      <MainBlog />
+    </Provider>
   </React.StrictMode>
 )
