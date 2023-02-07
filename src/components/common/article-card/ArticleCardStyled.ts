494eeaaf4dd7df863b913f6ae0fd0827fef71701
D3press3d <anthonyrosman20@gmail.com>
2023-02-07 12:56:35 -0400

    :lipstick: add responsive

--- src/components/common/article-card/ArticleCardStyled.ts
+++ src/components/common/article-card/ArticleCardStyled.ts
@@ -3,7 +3,10 @@ import styled from '@emotion/styled'
 export const ArticleCardStyled = styled.article`
   display: grid;
   grid-template-columns: 60% 40%;
+  gap: 10px;
+  align-items: center;
   background-color: var(--white);
+  text-overflow: ellipsis;
   width: 100%;
   border-radius: 10px;
   padding: 20px 30px;
@@ -19,18 +22,24 @@ export const ArticleCardStyled = styled.article`
   }
 `
 
-export const ArticleCardTitle = styled.h2`
-  font-size: 2.6rem;
-  color: var(--dark-blue-lighten);
-  line-height: 130%;
-`
-
 export const ArticleDate = styled.p`
   text-transform: uppercase;
   color: var(--gray-500);
   text-align: center;
   font-size: 13px;
   display: inline-block;
+`
+
+export const ArticleCardTitle = styled.h2`
+  font-size: 2.6rem;
+  color: var(--dark-blue-lighten);
+  line-height: 130%;
+`
+
+export const ArticleInfo = styled.p`
+  @media (max-width: 480px) {
+    display: none;
+  }
 `
 
 export const ArticleLeftSide = styled.div`
@@ -49,7 +58,7 @@ export const ArticleImage = styled.img`
   width: 100%;
   margin-left: auto;
   border-radius: 10px;
-  min-height: 100%;
+  max-height: 211px;
   object-fit: cover;
 
   @media (max-width: 768px) {
