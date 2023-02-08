16882a399b198adc971c81d1998158746b99d490
D3press3d <anthonyrosman20@gmail.com>
2023-02-08 19:00:18 -0400

    :art: add new footer component

--- src/MainBlog.tsx
+++ src/MainBlog.tsx
@@ -1,11 +1,13 @@
 import { Navbar } from './components/Navbar'
 import { MainContent } from './components/MainContent'
+import { Footer } from './components/Footer'
 
 function App (): JSX.Element {
   return (
     <>
       <Navbar />
       <MainContent />
+      <Footer/>
     </>
   )
 }
